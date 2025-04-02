64ef6dfede93c7e7bd675b801120dfbcc57b6420
Noble Lens <codingthuglocalhost@gmail.com>
2025-04-02 18:45:59 -0400

    After looking at my contract, I realize this was a great chance to implement what I just learned, and it's emitting events. I added an event emitter, so I'd know when someone added a new name and bio

--- submissions/CodingThug/SaveMyName.sol
+++ submissions/CodingThug/SaveMyName.sol
@@ -5,9 +5,22 @@ pragma solidity >=0.7.0 <0.9.0;
 contract SaveMyName{
   string public name;
   string public smallBio;
+  address public owner;
 
-  function saveName(string memory newName, string memory newBio) public {
+  event PersonCreated(string, newName, string newBio);
+
+  constructor(){
+    owner = msg.sender;
+  }
+
+  modifier onlyOwner(){
+    require(msg.sender == owner, "Now you know damn well, you are not the owner");
+    _;
+  }
+
+  function saveName(string memory newName, string memory newBio) public onlyOwner {
     name = newName;
     smallBio = newBio;
+    emit PersonCreated(newName, newBio);
   }
 }
